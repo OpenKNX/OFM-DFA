f0713e7191d0c64efbbfb735968862b9bc341ea8
Cornelius Köpp <cornelius.koepp@fhdw.de>
2026-04-25 20:39:12 +0200

    Refactor(Param-Calc): `_transPRI` -> `DFA_ad___` -> `_ParamDFA_ad___`
    * Refactor: Calculate Transition Parameters
    * Refactor: Calculate Transition Parameters / Replace _transPRI

--- src/DfaChannel.h
+++ src/DfaChannel.h
@@ -43,7 +43,6 @@ class DfaChannel : public OpenKNX::Channel
   private:
     static const uint8_t _magicWord[4];
 
-    static const uint16_t _transPRI[DFA_DEF_STATES_COUNT][DFA_DEF_INPUTS_WITH_T_COUNT];
     static const uint16_t _timeoutPRI[DFA_DEF_STATES_COUNT];
 
     // is enabled in ETS?
